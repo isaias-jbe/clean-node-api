31019fdd47d9df50ce715bb84892ca6297840064
Isaias <isaias.almeida@tallos.com.br>
2023-01-07 12:27:23 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -17,7 +17,7 @@ const makeAddAccount = (): AddAccount => {
       const fakeAccount = {
         id: 'valid_id',
         name: 'valid_name',
-        email: 'valid_email',
+        email: 'valid_email@test.com',
         password: 'valid_password'
       }
       return fakeAccount
@@ -198,6 +198,26 @@ describe('SignUp Controller', () => {
       name: 'any_name',
       email: 'any_email@test.com',
       password: 'any_password'
+    })
+  })
+
+  test('Should return 200 if valid date is provider', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@test.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@test.com',
+      password: 'valid_password'
     })
   })
 })
