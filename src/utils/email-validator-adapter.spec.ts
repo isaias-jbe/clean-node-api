dbda1a2d86a48d6b4e22dc3acb9fe3babf72727f
Isaias <isaias.almeida@tallos.com.br>
2023-01-09 21:11:20 -0300

    test: ensure EmailValidatorAdapter return false if validator returns false

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,32 @@
 import { EmailValidatorAdapter } from './email-validator'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
+
+interface SutType {
+  sut: EmailValidatorAdapter
+}
+
+const makeSut = (): SutType => {
+  const sut = new EmailValidatorAdapter()
+  return { sut }
+}
 
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const { sut } = makeSut()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
+  })
+
+  test('Should return true is validator returns true', () => {
+    const { sut } = makeSut()
+    const isValid = sut.isValid('valid_email@mail.com')
+    expect(isValid).toBe(true)
   })
 })
